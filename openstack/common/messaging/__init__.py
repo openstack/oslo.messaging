e8a0756a3a518a980bc4909e3688429a429fc4d2
Mark McLoughlin <markmc@redhat.com>
2013-05-11 13:57:51 +0100

    Add top-level convenience aliases
    This means you can do e.g.
    
      from openstack.common import messaging
    
      target = messaging.Target(...)
    
      transport = messaging.get_transport(...)
    
      class Client(messaging.RPCClient):
         ...
    
    rather than e.g.
    
      from openstack.common.messaging.rpc import client
      from openstack.common.messaging import target
      from openstack.common.messaging import transport
    
      target = target.Target(...)
    
      transport = transport.get_transport(...)
    
      class Client(client.RPCClient):
         ...

--- openstack/common/messaging/__init__.py
+++ openstack/common/messaging/__init__.py
@@ -12,3 +12,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+from .rpc.client import RPCClient
+from .rpc.server import BlockingRPCServer
+from .target import Target
+from .transport import get_transport
