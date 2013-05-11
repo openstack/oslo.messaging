7c3697f77b84045f9f9d06388b59c41bc8ae2cbe
Mark McLoughlin <markmc@redhat.com>
2013-05-11 13:18:43 +0100

    Make drivers list public again
    Make the list of drivers public via the o.c.m.drivers module.

--- openstack/common/messaging/_drivers/__init__.py
+++ openstack/common/messaging/_drivers/__init__.py
@@ -12,28 +12,3 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
-NAMESPACE = 'openstack.common.messaging.drivers'
-
-
-def _driver(module, name):
-    return 'openstack.common.messaging._drivers.%s:%s' % (module, name)
-
-_RABBIT_DRIVER = _driver('rabbit', 'RabbitDriver')
-_QPID_DRIVER = _driver('qpid', 'QpidDriver')
-_ZMQ_DRIVER = _driver('zmq', 'ZmqDriver')
-
-
-TRANSPORT_DRIVERS = [
-    'rabbit = ' + _RABBIT_DRIVER,
-    'qpid = ' + _QPID_DRIVER,
-    'zmq = ' + _ZMQ_DRIVER,
-
-    # To avoid confusion
-    'kombu = ' + _RABBIT_DRIVER,
-
-    # For backwards compat
-    'openstack.common.rpc.impl_kombu = ' + _RABBIT_DRIVER,
-    'openstack.common.rpc.impl_qpid = ' + _QPID_DRIVER,
-    'openstack.common.rpc.impl_zmq = ' + _ZMQ_DRIVER,
-]
--- openstack/common/messaging/transport.py
+++ openstack/common/messaging/transport.py
@@ -21,7 +21,7 @@ import urlparse
 from oslo.config import cfg
 from stevedore import driver
 
-from openstack.common.messaging import _drivers as drivers
+from openstack.common.messaging import drivers
 
 _transport_opts = [
     cfg.StrOpt('transport_url',
