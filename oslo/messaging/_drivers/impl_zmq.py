2e3b8878c9a9b181962365c78d79d08af766e201
zhangjl <zhangjl@awcloud.com>
2014-04-02 12:28:14 +0800

    Oslo-messaging-zmq-receiver cannot recive any messages
    In Icehouse b3 and rc1, when we use impl_zmq as
    rpc_backend in nova, nova cannot send any messages
    throught impl_zmq to oslo-messaging-zmq-receiver.
    The reason is we send two params to serialize_msg
    method but it only can recive one.To fix this
    problem, I removed the second param which not
    used when we call the method serialize_msg.
    
    Change-Id: I2e7546147affd9493f425fd879169932ef206d48
    Closes-bug: #1301132

--- oslo/messaging/_drivers/impl_zmq.py
+++ oslo/messaging/_drivers/impl_zmq.py
@@ -230,7 +230,7 @@ class ZmqClient(object):
                            (msg_id, topic, 'cast', _serialize(data))))
             return
 
-        rpc_envelope = rpc_common.serialize_msg(data[1], envelope)
+        rpc_envelope = rpc_common.serialize_msg(data[1])
         zmq_msg = moves.reduce(lambda x, y: x + y, rpc_envelope.items())
         self.outq.send(map(bytes,
                        (msg_id, topic, 'impl_zmq_v2', data[0]) + zmq_msg))
