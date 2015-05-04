e888daafee3bb59f714d219bf69646f273b930b1
Victor Sergeyev <vsergeyev@mirantis.com>
2015-05-04 16:06:32 +0300

    Add RequestContextSerializer
    This serializer available (with some differences) in ceilometer,
    cinder, designate, heat, ironic, magnum, manila, neutron, nova, trove.
    So we can move it to the common code and re-use (or inherit from it) in
    OpenStack projects
    
    Change-Id: I0d68b1d98c2214a5d45b65146ac2d19e5f6f5953

--- oslo_messaging/serializer.py
+++ oslo_messaging/serializer.py
@@ -12,12 +12,14 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-__all__ = ['Serializer', 'NoOpSerializer', 'JsonPayloadSerializer']
+__all__ = ['Serializer', 'NoOpSerializer', 'JsonPayloadSerializer',
+           'RequestContextSerializer']
 
 """Provides the definition of a message serialization handler"""
 
 import abc
 
+from oslo_context import context as common_context
 from oslo_serialization import jsonutils
 import six
 
@@ -59,6 +61,28 @@ class Serializer(object):
         :param ctxt: Request context dictionary
         :returns: Deserialized form of entity
         """
+
+
+class RequestContextSerializer(Serializer):
+
+    def __init__(self, base):
+        self._base = base
+
+    def serialize_entity(self, context, entity):
+        if not self._base:
+            return entity
+        return self._base.serialize_entity(context, entity)
+
+    def deserialize_entity(self, context, entity):
+        if not self._base:
+            return entity
+        return self._base.deserialize_entity(context, entity)
+
+    def serialize_context(self, context):
+        return context.to_dict()
+
+    def deserialize_context(self, context):
+        return common_context.RequestContext.from_dict(context)
 
 
 class NoOpSerializer(Serializer):
