e7d6e92ea7e1206eed2016d64c95e78075711aee
Oleksii Zamiatin <ozamiatin@mirantis.com>
2016-01-28 14:30:04 +0200

    [zmq] Fix slow down
    Restore use_pub_sub to default True.
    Fix broker closed by mistake.
    
    Change-Id: Ibc370d401d233059f7cbe8e7d5b15136910c8be7

--- oslo_messaging/_drivers/impl_zmq.py
+++ oslo_messaging/_drivers/impl_zmq.py
@@ -70,7 +70,7 @@ zmq_opts = [
                help='Expiration timeout in seconds of a name service record '
                     'about existing target ( < 0 means no timeout).'),
 
-    cfg.BoolOpt('use_pub_sub', default=False,
+    cfg.BoolOpt('use_pub_sub', default=True,
                 help='Use PUB/SUB pattern for fanout methods. '
                      'PUB/SUB always uses proxy.'),
 
