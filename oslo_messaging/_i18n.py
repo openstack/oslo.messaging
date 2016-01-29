e32560e9ebb2733b115b112d1064ec9248f67b8a
Andreas Jaeger <aj@suse.com>
2016-01-29 11:34:55 +0100

    Update translation setup
    Follow new infra setup for translations, see spec
    http://specs.openstack.org/openstack-infra/infra-specs/specs/translation_setup.html
    for full details.
    
    This basically renames
    oslo.messaging/locale/oslo.messaging.pot to
    oslo_messaging/locale/oslo_messaging.pot. For this we need to update
    setup.cfg.
    
    Update also domain name in i18n.py.
    
    Change-Id: Idf5300224f572d29953261ac998e7bf32a12822d

--- oslo_messaging/_i18n.py
+++ oslo_messaging/_i18n.py
@@ -19,7 +19,7 @@ See http://docs.openstack.org/developer/oslo.i18n/usage.html
 import oslo_i18n
 
 
-_translators = oslo_i18n.TranslatorFactory(domain='oslo.messaging')
+_translators = oslo_i18n.TranslatorFactory(domain='oslo_messaging')
 
 # The primary translation function using the well-known name "_"
 _ = _translators.primary
