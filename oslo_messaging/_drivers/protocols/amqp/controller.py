f42d88617c8acfb751f5f0538bdba0d158d3a363
Kenneth Giusti <kgiusti@gmail.com>
2016-03-08 14:46:00 -0500

    Fix the driver shutdown/failover logic
    Re-work much of the shutdown/failover logic.  Failover due to a broker
    hang or broker-initiated shutdown works properly and new unit tests
    have been added for these cases. The shutdown code has been fixed to
    correctly shutdown the background thread and release resources.
    Socket failure and link failure handling has been improved.  Fork
    handling code works properly.
    
    Closes-Bug: #1554046
    Change-Id: I5aea2f8212e60964fb78eae30078fcf672d7f447

--- oslo_messaging/_drivers/protocols/amqp/controller.py
+++ oslo_messaging/_drivers/protocols/amqp/controller.py
@@ -50,6 +50,46 @@ class Task(object):
         """This method will be run on the eventloop thread."""
 
 
+class Sender(pyngus.SenderEventHandler):
+    """A single outgoing link to a given address"""
+    def __init__(self, address):
+        self._address = address
+        self._link = None
+
+    def attach(self, connection):
+        # open a link to the destination
+        sname = "Producer-%s:src=%s:tgt=%s" % (uuid.uuid4().hex,
+                                               self._address,
+                                               self._address)
+        self._link = connection.create_sender(name=sname,
+                                              source_address=self._address,
+                                              target_address=self._address)
+        self._link.open()
+
+    def detach(self):
+        # close the link
+        if self._link:
+            self._link.close()
+
+    def destroy(self):
+        # drop reference to link. The link will be freed when the
+        # connection is destroyed
+        self._link = None
+
+    def send(self, message, callback):
+        # send message out the link, invoke callback when acked
+        self._link.send(message, delivery_callback=callback)
+
+    def sender_remote_closed(self, sender_link, pn_condition):
+        LOG.debug("sender_remote_closed condition=%s", pn_condition)
+        sender_link.close()
+
+    def sender_failed(self, sender_link, error):
+        """Protocol error occurred."""
+        LOG.error(_LE("Outgoing link to %(addr) failed. error=%(error)"),
+                  {"addr": self._address, "error": error})
+
+
 class Replies(pyngus.ReceiverEventHandler):
     """This is the receiving link for all reply messages.  Messages are routed
     to the proper Listener's incoming queue using the correlation-id header in
@@ -73,8 +113,13 @@ class Replies(pyngus.ReceiverEventHandler):
         self._credit = 0
         self._receiver.open()
 
+    def detach(self):
+        # close the link
+        self._receiver.close()
+
     def destroy(self):
-        self._correlation = None
+        # drop reference to link. Link will be freed when the connection is
+        # released.
         self._receiver = None
 
     def ready(self):
@@ -119,11 +164,17 @@ class Replies(pyngus.ReceiverEventHandler):
         """This is a Pyngus callback, invoked by Pyngus when the peer of this
         receiver link has initiated closing the connection.
         """
-        # TODO(kgiusti) Unclear if this error will ever occur (as opposed to
-        # the Connection failing instead).  Log for now, possibly implement a
-        # recovery strategy if necessary.
-        LOG.error(_LE("Reply subscription closed by peer: %s"),
-                  (pn_condition or "no error given"))
+        # TODO(kgiusti)  Log for now, possibly implement a recovery strategy if
+        # necessary.
+        if pn_condition:
+            LOG.error(_LE("Reply subscription closed by peer: %s"),
+                      pn_condition)
+        receiver.close()
+
+    def receiver_failed(self, receiver_link, error):
+        """Protocol error occurred."""
+        LOG.error(_LE("Link to reply queue %(addr) failed. error=%(error)"),
+                  {"addr": self._address, "error": error})
 
     def message_received(self, receiver, message, handle):
         """This is a Pyngus callback, invoked by Pyngus when a new message
@@ -162,14 +213,13 @@ class Server(pyngus.ReceiverEventHandler):
         self._incoming = incoming
         self._addresses = addresses
         self._capacity = 500   # credit per link
-        self._receivers = None
+        self._receivers = []
         self._id = subscription_id
 
     def attach(self, connection):
         """Create receiver links over the given connection for all the
         configured addresses.
         """
-        self._receivers = []
         for a in self._addresses:
             props = {"snd-settle-mode": "settled"}
             rname = "Consumer-%s:src=%s:tgt=%s" % (uuid.uuid4().hex, a, a)
@@ -187,8 +237,18 @@ class Server(pyngus.ReceiverEventHandler):
             r.open()
             self._receivers.append(r)
 
-    def destroy(self):
-        self._receivers = None
+    def detach(self):
+        # close the links
+        for receiver in self._receivers:
+            receiver.close()
+
+    def reset(self):
+        # destroy the links, but keep the addresses around since we may be
+        # failing over.  Since links are destroyed, this cannot be called from
+        # any of the following ReceiverLink callbacks.
+        for r in self._receivers:
+            r.destroy()
+        self._receivers = []
 
     # Pyngus ReceiverLink event callbacks:
 
@@ -196,12 +256,19 @@ class Server(pyngus.ReceiverEventHandler):
         """This is a Pyngus callback, invoked by Pyngus when the peer of this
         receiver link has initiated closing the connection.
         """
-        vals = {
-            "addr": receiver.source_address or receiver.target_address,
-            "err_msg": pn_condition or "no error given"
-        }
-        LOG.error(_LE("Server subscription %(addr)s closed "
-                      "by peer: %(err_msg)s"), vals)
+        if pn_condition:
+            vals = {
+                "addr": receiver.source_address or receiver.target_address,
+                "err_msg": pn_condition
+            }
+            LOG.error(_LE("Server subscription %(addr)s closed "
+                          "by peer: %(err_msg)s"), vals)
+        receiver.close()
+
+    def receiver_failed(self, receiver_link, error):
+        """Protocol error occurred."""
+        LOG.error(_LE("Listener link queue %(addr) failed. error=%(error)"),
+                  {"addr": self._address, "error": error})
 
     def message_received(self, receiver, message, handle):
         """This is a Pyngus callback, invoked by Pyngus when a new message
@@ -323,22 +390,16 @@ class Controller(pyngus.ConnectionEventHandler):
         self._tasks.put(task)
         self._schedule_task_processing()
 
-    def shutdown(self, wait=True, timeout=None):
+    def shutdown(self, timeout=None):
         """Shutdown the messaging service."""
+        LOG.info(_LI("Shutting down the AMQP 1.0 connection"))
         if self.processor:
+            self.processor.wakeup(lambda: self._start_shutdown())
             LOG.debug("Waiting for eventloop to exit")
-            self.processor.shutdown(wait, timeout)
+            self.processor.join(timeout)
+            self._hard_reset()
+            self.processor.destroy()
             self.processor = None
-        self._tasks = None
-        self._senders = None
-        for servers in self._servers.values():
-            for server in servers.values():
-                server.destroy()
-        self._servers.clear()
-        self._socket_connection = None
-        if self._replies:
-            self._replies.destroy()
-            self._replies = None
         LOG.debug("Eventloop exited, driver shut down")
 
     # The remaining methods are reserved to run from the eventloop thread only!
@@ -426,16 +487,10 @@ class Controller(pyngus.ConnectionEventHandler):
     def _sender(self, address):
         # if we already have a sender for that address, use it
         # else establish the sender and cache it
-        if address in self._senders:
-            sender = self._senders[address]
-        else:
-            sname = "Producer-%s:src=%s:tgt=%s" % (uuid.uuid4().hex,
-                                                   address, address)
-            conn = self._socket_connection.connection
-            sender = conn.create_sender(source_address=address,
-                                        target_address=address,
-                                        name=sname)
-            sender.open()
+        sender = self._senders.get(address)
+        if sender is None:
+            sender = Sender(address)
+            sender.attach(self._socket_connection.connection)
             self._senders[address] = sender
         return sender
 
@@ -446,7 +501,7 @@ class Controller(pyngus.ConnectionEventHandler):
         """
         address = str(addr)
         message.address = address
-        self._sender(address).send(message, delivery_callback=callback)
+        self._sender(address).send(message, callback)
 
     def _server_address(self, target):
         return self._concatenate([self.server_request_prefix,
@@ -540,17 +595,24 @@ class Controller(pyngus.ConnectionEventHandler):
                 self._replies and self._replies.ready())
 
     def _start_shutdown(self):
-        """Called when the driver destroys the controller, this method attempts
-        to cleanly close the AMQP connection to the peer.
+        """Called when the application is closing the transport.
+        Attempt to cleanly flush/close all links.
         """
-        LOG.info(_LI("Shutting down AMQP connection"))
         self._closing = True
-        if self._socket_connection.connection.active:
+        if (self._socket_connection
+                and self._socket_connection.connection
+                and self._socket_connection.connection.active):
             # try a clean shutdown
+            for sender in self._senders.values():
+                sender.detach()
+            for servers in self._servers.values():
+                for server in servers.values():
+                    server.detach()
+            self._replies.detach()
             self._socket_connection.connection.close()
         else:
             # don't wait for a close from the remote, may never happen
-            self._complete_shutdown()
+            self.processor.shutdown()
 
     # reply link active callback:
 
@@ -567,7 +629,7 @@ class Controller(pyngus.ConnectionEventHandler):
 
     def socket_error(self, error):
         """Called by eventloop when a socket error occurs."""
-        LOG.debug("Socket failure: %s", error)
+        LOG.error(_LE("Socket failure: %s"), error)
         self._handle_connection_loss()
 
     # Pyngus connection event callbacks (and their helpers), all invoked from
@@ -612,14 +674,12 @@ class Controller(pyngus.ConnectionEventHandler):
         """This is a Pyngus callback, invoked by Pyngus when the peer has
         requested that the connection be closed.
         """
-        if not self._closing:
-            # The messaging service/broker is trying to shut down the
-            # connection. Acknowledge the close, and try to reconnect/failover
-            # later once the connection has closed (connection_closed is
-            # called).
-            LOG.info(_LI("Connection closed by peer: %s"),
-                     reason or "no reason given")
-            self._socket_connection.connection.close()
+        # The messaging service/broker is trying to shut down the
+        # connection. Acknowledge the close, and try to reconnect/failover
+        # later once the connection has closed (connection_closed is called).
+        if reason:
+            LOG.info(_LI("Connection closed by peer: %s"), reason)
+        self._socket_connection.connection.close()
 
     def sasl_done(self, connection, pn_sasl, outcome):
         """This is a Pyngus callback invoked when the SASL handshake
@@ -635,28 +695,19 @@ class Controller(pyngus.ConnectionEventHandler):
                    'username': self.hosts.current.username})
         # connection failure will be handled later
 
-    def _complete_shutdown(self):
-        """The AMQP Connection has closed, and the driver shutdown is complete.
-        Clean up controller resources and exit.
-        """
-        self._socket_connection.close()
-        self.processor.shutdown()
-        LOG.info(_LI("Messaging has shutdown"))
-
     def _handle_connection_loss(self):
         """The connection to the messaging service has been lost.  Try to
-        reestablish the connection/failover.
+        reestablish the connection/failover if not shutting down the driver.
         """
         if self._closing:
             # we're in the middle of shutting down the driver anyways,
             # just consider it done:
-            self._complete_shutdown()
+            self.processor.shutdown()
         else:
             # for some reason, we've lost the connection to the messaging
             # service.  Try to re-establish the connection:
             if not self._reconnecting:
                 self._reconnecting = True
-                self._replies = None
                 LOG.info(_LI("delaying reconnect attempt for %d seconds"),
                          self._delay)
                 self.processor.schedule(lambda: self._do_reconnect(),
@@ -668,14 +719,29 @@ class Controller(pyngus.ConnectionEventHandler):
         """Invoked on connection/socket failure, failover and re-connect to the
         messaging service.
         """
-        # note well: since this method destroys the connection, it cannot be
-        # invoked directly from a pyngus callback.  Use processor.schedule() to
-        # run this method on the main loop instead.
         if not self._closing:
+            self._hard_reset()
             self._reconnecting = False
-            self._senders = {}
-            self._socket_connection.reset()
             host = self.hosts.next()
             LOG.info(_LI("Reconnecting to: %(hostname)s:%(port)s"),
                      {'hostname': host.hostname, 'port': host.port})
             self._socket_connection.connect(host)
+
+    def _hard_reset(self):
+        """Reset the controller to its pre-connection state"""
+        # note well: since this method destroys the connection, it cannot be
+        # invoked directly from a pyngus callback.  Use processor.schedule() to
+        # run this method on the main loop instead.
+        for sender in self._senders.values():
+            sender.destroy()
+        self._senders.clear()
+        for servers in self._servers.values():
+            for server in servers.values():
+                # discard links, but keep servers around to re-attach if
+                # failing over
+                server.reset()
+        if self._replies:
+            self._replies.destroy()
+            self._replies = None
+        if self._socket_connection:
+            self._socket_connection.reset()
