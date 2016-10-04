cb3af2167fdb1ba8977163217169f0beedcae5e7
Gevorg Davoian <gdavoian@mirantis.com>
2016-10-04 12:36:13 +0300

    [zmq] Maintain several redis hosts
    This patch makes it possible to maintain several redis hosts at
    once in order to increase driver's reliability and fault tolerance.
    
    Change-Id: Id6f63a4bb67a39340a74d16144c79028c7af245d

--- oslo_messaging/tests/drivers/zmq/test_zmq_transport_url.py
+++ oslo_messaging/tests/drivers/zmq/test_zmq_transport_url.py
@@ -22,7 +22,6 @@ from oslo_messaging._drivers.zmq_driver.matchmaker import zmq_matchmaker_redis
 from oslo_messaging._drivers.zmq_driver import zmq_async
 from oslo_messaging.tests import utils as test_utils
 
-
 zmq = zmq_async.import_zmq()
 
 
@@ -44,7 +43,7 @@ class TestZmqTransportUrl(test_utils.BaseTestCase):
                       driver.matchmaker.__class__)
         self.assertEqual('zmq', driver.matchmaker.url.transport)
 
-    def test_error_name(self):
+    def test_error_url(self):
         self.assertRaises(common.RPCException, self.setup_url, "zmq+error:///")
 
     def test_dummy_url(self):
@@ -59,30 +58,44 @@ class TestZmqTransportUrl(test_utils.BaseTestCase):
                       driver.matchmaker.__class__)
         self.assertEqual('zmq+redis', driver.matchmaker.url.transport)
 
-    def test_redis_url_no_creds(self):
-        driver, url = self.setup_url("zmq+redis://host:65123/")
-        self.assertIs(zmq_matchmaker_redis.MatchmakerRedis,
+    def test_sentinel_url(self):
+        driver, url = self.setup_url("zmq+sentinel:///")
+        self.assertIs(zmq_matchmaker_redis.MatchmakerSentinel,
                       driver.matchmaker.__class__)
-        self.assertEqual('zmq+redis', driver.matchmaker.url.transport)
-        self.assertEqual("host", driver.matchmaker.standalone_redis["host"])
-        self.assertEqual(65123, driver.matchmaker.standalone_redis["port"])
+        self.assertEqual('zmq+sentinel', driver.matchmaker.url.transport)
 
-    def test_redis_url_no_port(self):
-        driver, url = self.setup_url("zmq+redis://:p12@host:65123/")
+    def test_host_with_credentials_url(self):
+        driver, url = self.setup_url("zmq://:password@host:60000/")
+        self.assertIs(zmq_matchmaker_redis.MatchmakerRedis,
+                      driver.matchmaker.__class__)
+        self.assertEqual('zmq', driver.matchmaker.url.transport)
+        self.assertEqual(
+            [{"host": "host", "port": 60000, "password": "password"}],
+            driver.matchmaker._redis_hosts
+        )
+
+    def test_redis_multiple_hosts_url(self):
+        driver, url = self.setup_url(
+            "zmq+redis://host1:60001,host2:60002,host3:60003/"
+        )
         self.assertIs(zmq_matchmaker_redis.MatchmakerRedis,
                       driver.matchmaker.__class__)
         self.assertEqual('zmq+redis', driver.matchmaker.url.transport)
-        self.assertEqual("host", driver.matchmaker.standalone_redis["host"])
-        self.assertEqual(65123, driver.matchmaker.standalone_redis["port"])
-        self.assertEqual("p12", driver.matchmaker.standalone_redis["password"])
+        self.assertEqual(
+            [{"host": "host1", "port": 60001, "password": None},
+             {"host": "host2", "port": 60002, "password": None},
+             {"host": "host3", "port": 60003, "password": None}],
+            driver.matchmaker._redis_hosts
+        )
 
     def test_sentinel_multiple_hosts_url(self):
         driver, url = self.setup_url(
-            "zmq+redis://sentinel1:20001,sentinel2:20001,sentinel3:20001/")
-        self.assertIs(zmq_matchmaker_redis.MatchmakerRedis,
+            "zmq+sentinel://host1:20001,host2:20002,host3:20003/"
+        )
+        self.assertIs(zmq_matchmaker_redis.MatchmakerSentinel,
                       driver.matchmaker.__class__)
-        self.assertEqual('zmq+redis', driver.matchmaker.url.transport)
-        self.assertEqual(3, len(driver.matchmaker.sentinel_hosts))
-        expected = [("sentinel1", 20001), ("sentinel2", 20001),
-                    ("sentinel3", 20001)]
-        self.assertEqual(expected, driver.matchmaker.sentinel_hosts)
+        self.assertEqual('zmq+sentinel', driver.matchmaker.url.transport)
+        self.assertEqual(
+            [("host1", 20001), ("host2", 20002), ("host3", 20003)],
+            driver.matchmaker._sentinel_hosts
+        )
