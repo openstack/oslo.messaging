openstack/oslo.cache

pbr.version.VersionInfo needs package name (oslo.xyz and not oslo_xyz)
 We need to pass into VersionInfo what one would expect from running:
 setup.py --name

 Right now we pass in say oslo_context and pbr fails if there is no .git in
  the python source tree

  Closes-Bug: #1662266

Change-Id: I6a4c2875d45609c5bc7326273a2e9b45fde6c085

oslo_cache/version.py
@@ -15,4 +15,4 @@
 
 import pbr.version
 
-version_info = pbr.version.VersionInfo('oslo_cache')
+version_info = pbr.version.VersionInfo('oslo.cache')

openstack/oslo.messaging

Change-Id: I45d2edddfd516f65e1589e9b7816f1256e3f0d24
(cherry picked from commit f33bb02c8239eed78c7e9ef2f45cef5c271bee75)

oslo_messaging/version.py
@@ -15,4 +15,4 @@
 
 import pbr.version
 
-version_info = pbr.version.VersionInfo('oslo_messaging')
+version_info = pbr.version.VersionInfo('oslo.messaging')