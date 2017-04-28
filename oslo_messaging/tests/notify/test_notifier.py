761f4fc02bf9732218efe895fa4924da45077a8e
Dmitry Mescheryakov <dmescheryakov@mirantis.com>
2017-04-28 17:53:33 +0400

    Fix notification tests not unmocking logging
    Right now two tests mock logging module globally which leads to
    another test failure (TestRoutingNotifier.test_notify_filtered)
    on my env.
    
    Change-Id: Ifa47ee96dae79ea44dbe17a2d125401589ff13dd
    (cherry picked from commit eb4f5c4fa03f34918e70af52773e95cccf51a039)

--- oslo_messaging/tests/notify/test_notifier.py
+++ oslo_messaging/tests/notify/test_notifier.py
@@ -320,6 +320,8 @@ class TestLogNotifier(test_utils.BaseTestCase):
 
         mock_utcnow.return_value = datetime.datetime.utcnow()
 
+        logger = mock.Mock()
+
         message = {
             'message_id': str(message_id),
             'publisher_id': 'test.localhost',
@@ -329,15 +331,15 @@ class TestLogNotifier(test_utils.BaseTestCase):
             'timestamp': str(timeutils.utcnow()),
         }
 
-        logger = mock.Mock()
-        logging.getLogger = mock.Mock()
-        logging.getLogger.return_value = logger
+        with mock.patch.object(logging, 'getLogger') as gl:
+            gl.return_value = logger
 
-        notifier.info({}, 'test.notify', 'bar')
+            notifier.info({}, 'test.notify', 'bar')
 
-        uuid.uuid4.assert_called_once_with()
-        logging.getLogger.assert_called_once_with('oslo.messaging.'
-                                                  'notification.test.notify')
+            uuid.uuid4.assert_called_once_with()
+            logging.getLogger.assert_called_once_with(
+                'oslo.messaging.notification.test.notify')
+
         logger.info.assert_called_once_with(JsonMessageMatcher(message))
 
     def test_sample_priority(self):
@@ -348,14 +350,15 @@ class TestLogNotifier(test_utils.BaseTestCase):
                                                   'notification.foo'))
         logger.sample = None
 
-        logging.getLogger = mock.Mock()
-        logging.getLogger.return_value = logger
-
         msg = {'event_type': 'foo'}
-        driver.notify(None, msg, "sample", None)
 
-        logging.getLogger.assert_called_once_with('oslo.messaging.'
-                                                  'notification.foo')
+        with mock.patch.object(logging, 'getLogger') as gl:
+            gl.return_value = logger
+
+            driver.notify(None, msg, "sample", None)
+
+            logging.getLogger.assert_called_once_with('oslo.messaging.'
+                                                      'notification.foo')
 
     def test_mask_passwords(self):
         # Ensure that passwords are masked with notifications
