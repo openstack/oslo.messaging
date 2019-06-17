54356899da5d4a0997abb6819f184a27929cc0e9
Hervé Beraud <hberaud@redhat.com>
2019-06-17 17:58:47 +0200

    fix typos
    Change-Id: Id11db4113c9b1c3add602192c1e915218704ef27

--- oslo_messaging/_drivers/impl_rabbit.py
+++ oslo_messaging/_drivers/impl_rabbit.py
@@ -535,7 +535,7 @@ class Connection(object):
         # expected waiting the events drain, we start heartbeat_check and
         # retrieve the server heartbeat packet only two times more than
         # the minimum required for the heartbeat works
-        # (heatbeat_timeout/heartbeat_rate/2.0, default kombu
+        # (heartbeat_timeout/heartbeat_rate/2.0, default kombu
         # heartbeat_rate is 2)
         self._heartbeat_wait_timeout = (
             float(self.heartbeat_timeout_threshold) /
@@ -560,7 +560,7 @@ class Connection(object):
 
         # NOTE(sileht): value chosen according the best practice from kombu
         # http://kombu.readthedocs.org/en/latest/reference/kombu.common.html#kombu.common.eventloop
-        # For heatbeat, we can set a bigger timeout, and check we receive the
+        # For heartbeat, we can set a bigger timeout, and check we receive the
         # heartbeat packets regulary
         if self._heartbeat_supported_and_enabled():
             self._poll_timeout = self._heartbeat_wait_timeout
@@ -953,7 +953,7 @@ class Connection(object):
                                  "occurred, trying to reconnect: %s", exc)
                         self.ensure_connection()
                 except Exception:
-                    LOG.warning("Unexpected error during heartbeart "
+                    LOG.warning("Unexpected error during heartbeat "
                                 "thread processing, retrying...")
                     LOG.debug('Exception', exc_info=True)
 
