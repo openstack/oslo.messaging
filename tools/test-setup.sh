dfcc4b0010d39d1f16be413bb1c3a4808718cd19
Andreas Jaeger <aj@suse.com>
2020-06-17 16:53:03 +0200

    Simplify tools/test-setup.sh
    test-setup.sh is called after bindep is run in CI, and tox is not setup.
    
    Simplify the script so that it only does what's needed - install
    qdrouterd for Ubuntu from qpid/testing.
    
    Install the other packages using bindep, add test to amqp1 profile.
    
    Add amqp1 to bindep_profiles where needed for scenario tests, previously
    test-setup.sh added it everywhere.
    
    NOTE(elod.illes): skip annotation removed from test_impl_rabbit.py, as
    kombu is constrained in ussuri with the version that does not need any
    test to skip.
    
    Change-Id: Ic3a9e2c873619670edfbf71022d593f3cb5f70f2
    Related-Bug: #1885923
    (cherry picked from commit 7e406c312a6514e7ae377edb52b9e02b5bf37a7d)

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -6,21 +6,9 @@
 
 # This setup for amqp1 needs to be run by a user that can run sudo.
 
-function is_fedora {
-    [ -f /usr/bin/yum ] && cat /etc/*release | grep -q -e "Fedora"
-}
-
-# NOTE(sileht): we create the virtualenv only and use bindep directly
-# because tox doesn't have a quiet option...
-tox -ebindep --notest
-
-# TODO(ansmith) for now setup amqp1 dependencies for any profile.
-# Fix this when test-setup is passed environment profile setting.
-
-# NOTE(sileht): bindep return 1 if some packages have to be installed
-PACKAGES="$(.tox/bindep/bin/bindep -b -f bindep.txt amqp1 || true)"
-
-[ -n "$PACKAGES" ] || exit 0
+# qdrouterd needs to be installed from qpid/testing repo in Ubuntu.
+# bindep does not allow setting up another repo, so we just install
+# this package here.
 
 # inspired from project-config install-distro-packages.sh
 if apt-get -v >/dev/null 2>&1 ; then
@@ -28,11 +16,5 @@ if apt-get -v >/dev/null 2>&1 ; then
     sudo apt-get -qq update
     sudo PATH=/usr/sbin:/sbin:$PATH DEBIAN_FRONTEND=noninteractive \
         apt-get -q --option "Dpkg::Options::=--force-confold" \
-        --assume-yes install $PACKAGES
-elif emerge --version >/dev/null 2>&1 ; then
-    sudo emerge -uDNq --jobs=4 @world
-    sudo PATH=/usr/sbin:/sbin:$PATH emerge -q --jobs=4 $PACKAGES
-else
-    is_fedora && YUM=dnf || YUM=yum
-    sudo PATH=/usr/sbin:/sbin:$PATH $YUM install -y $PACKAGES
+        --assume-yes install qdrouterd
 fi
