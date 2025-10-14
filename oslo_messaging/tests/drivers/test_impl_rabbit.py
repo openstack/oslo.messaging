591c4f59ae6e6afbf3983c0661f03e49243c5eef
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-10-14 22:23:48 +0900

    Bump kombu to 4.6.8
    kombu 4.6.8 was released in 2020.
    
    Change-Id: I1e2fbb8c474ff33fd035119c70fb5f6f62eda683
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- oslo_messaging/tests/drivers/test_impl_rabbit.py
+++ oslo_messaging/tests/drivers/test_impl_rabbit.py
@@ -1048,19 +1048,15 @@ class RpcKombuHATestCase(test_utils.BaseTestCase):
             'kombu.connection.Connection.connection'))
         self.useFixture(fixtures.MockPatch(
             'kombu.connection.Connection.channel'))
-        # TODO(stephenfin): Drop hasattr when we drop support for kombo < 4.6.8
-        if hasattr(kombu.connection.Connection, '_connection_factory'):
-            self.useFixture(fixtures.MockPatch(
-                'kombu.connection.Connection._connection_factory'))
+        self.useFixture(fixtures.MockPatch(
+            'kombu.connection.Connection._connection_factory'))
 
         # starting from the first broker in the list
         url = oslo_messaging.TransportURL.parse(self.conf, None)
         self.connection = rabbit_driver.Connection(self.conf, url,
                                                    driver_common.PURPOSE_SEND)
-        # TODO(stephenfin): Remove when we drop support for kombo < 4.6.8
-        if hasattr(kombu.connection.Connection, 'connect'):
-            self.useFixture(fixtures.MockPatch(
-                'kombu.connection.Connection.connect'))
+        self.useFixture(fixtures.MockPatch(
+            'kombu.connection.Connection.connect'))
         self.addCleanup(self.connection.close)
 
     def test_ensure_four_retry(self):
