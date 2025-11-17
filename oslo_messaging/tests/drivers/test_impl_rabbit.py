openstack/oslo.messaging

commit 1fd461647f7f727dad9d4603abf0defe339d320f

Force creating non durable control exchange when a precondition failed
Precondition failed exception related to durable exchange
config may be triggered when a control exchange is shared
between services and when services try to create it with
configs that differ from each others. RabbitMQ will reject
the services that try to create it with a configuration
that differ from the one used first.

This kind of exception is not managed for now and services
can fails without handling this kind of issue.

These changes catch this kind exception to analyze if they
related to durable config. In this case we try to re-declare
the failing exchange/queue as non durable.

This problem can be easily reproduced by running a local RabbitMQ
server.

By setting the config below (sample.conf):

```
[DEFAULT]
transport_url = rabbit://localhost/
[OSLO_MESSAGING_RABBIT]
amqp_durable_queues = true
```

And by running our simulator twice:

```
$ tox -e venv --  python tools/simulator.py -d rpc-server -w 40
$ tox -e venv --  python tools/simulator.py --config-file ./sample.conf -d rpc-server -w 40
```

The first one will create a default non durable control exchange.
The second one will create the same default control exchange but as
durable.

Closes-Bug: #1953351
Change-Id: I27625b468c428cde6609730c8ab429c2c112d010

oslo_messaging/_drivers/impl_rabbit.py
@@ -254,10 +254,43 @@ class Consumer(object):
         self._declared_on = None
         self.exchange = kombu.entity.Exchange(
             name=exchange_name,
-            type=type,
+            type=self.type,
             durable=self.durable,
             auto_delete=self.exchange_auto_delete)
         self.enable_cancel_on_failover = enable_cancel_on_failover
+
+    def _declare_fallback(self, err, conn, consumer_arguments):
+        """Fallback by declaring a non durable queue.
+
+        When a control exchange is shared between services it is possible
+        that some service created first a non durable control exchange and
+        then after that an other service can try to create the same control
+        exchange but as a durable control exchange. In this case RabbitMQ
+        will raise an exception (PreconditionFailed), and then it will stop
+        our execution and our service will fail entirly. In this case we want
+        to fallback by creating a non durable queue to match the default
+        config.
+        """
+        if "PRECONDITION_FAILED - inequivalent arg 'durable'" in str(err):
+            LOG.info(
+                "[%s] Retrying to declare the exchange (%s) as "
+                "non durable", conn.connection_id, self.exchange_name)
+            self.exchange = kombu.entity.Exchange(
+                name=self.exchange_name,
+                type=self.type,
+                durable=False,
+                auto_delete=self.queue_auto_delete)
+            self.queue = kombu.entity.Queue(
+                name=self.queue_name,
+                channel=conn.channel,
+                exchange=self.exchange,
+                durable=False,
+                auto_delete=self.queue_auto_delete,
+                routing_key=self.routing_key,
+                queue_arguments=self.queue_arguments,
+                consumer_arguments=consumer_arguments
+            )
+            self.queue.declare()
 
     def declare(self, conn):
         """Re-declare the queue after a rabbit (re)connect."""
@@ -281,7 +314,18 @@ class Consumer(object):
         try:
             LOG.debug('[%s] Queue.declare: %s',
                       conn.connection_id, self.queue_name)
-            self.queue.declare()
+            try:
+                self.queue.declare()
+            except amqp_exec.PreconditionFailed as err:
+                # NOTE(hberaud): This kind of exception may be triggered
+                # when a control exchange is shared between services and
+                # when services try to create it with configs that differ
+                # from each others. RabbitMQ will reject the services
+                # that try to create it with a configuration that differ
+                # from the one used first.
+                LOG.warning(err)
+                self._declare_fallback(err, conn, consumer_arguments)
+
         except conn.connection.channel_errors as exc:
             # NOTE(jrosenboom): This exception may be triggered by a race
             # condition. Simply retrying will solve the error most of the time
@@ -1219,7 +1263,19 @@ class Connection(object):
         """Publish a message."""
 
         if not (exchange.passive or exchange.name in self._declared_exchanges):
-            exchange(self.channel).declare()
+            try:
+                exchange(self.channel).declare()
+            except amqp_exec.PreconditionFailed as err:
+                # NOTE(hberaud): This kind of exception may be triggered
+                # when a control exchange is shared between services and
+                # when services try to create it with configs that differ
+                # from each others. RabbitMQ will reject the services
+                # that try to create it with a configuration that differ
+                # from the one used first.
+                if "PRECONDITION_FAILED - inequivalent arg 'durable'" \
+                   in str(err):
+                    exchange.durable = False
+                    exchange(self.channel).declare()
             self._declared_exchanges.add(exchange.name)
 
         log_info = {'msg': msg,

releasenotes/notes/declare_fallback_durable_exchange-0db677de4fdf1e78.yaml
@@ -1,0 +1,5 @@
+---
+fixes:
+  - |
+    Force creating non durable control exchange when a precondition failed
+    related to config that differ occuring.

commit 8961eedc7141f79aea2342cba927df584e003d1b

rabbit: Fix ignored PreconditionFailed error
The change 1fd461647f7f727dad9d4603abf0defe339d320f introduced
additional fallback logic to conflicting durable flag within a single
cluster, but it accidentally made the driver ignore the other errors
classified as PreconditionFailed error.

One example of PreconditionFailed is cluster exchange limit which was
introduced in 4.1.4, and this error is now silently ignored due to
this ignorance.

Make sure that PreconditionFailed is raised unless we know it may be
possible caused by conflicting durable field.

Closes-Bug: #2131652
Change-Id: Ia315fc3e91c2f129025b74bc18b5a93c1d40b498
Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

oslo_messaging/_drivers/impl_rabbit.py
@@ -418,7 +418,7 @@ class Consumer:
         self.rabbit_stream_fanout = rabbit_stream_fanout
         self.next_stream_offset = "last"
 
-    def _declare_fallback(self, err, conn, consumer_arguments):
+    def _declare_fallback_nondurable(self, err, conn, consumer_arguments):
         """Fallback by declaring a non durable queue.
 
         When a control exchange is shared between services it is possible
@@ -430,26 +430,25 @@ class Consumer:
         to fallback by creating a non durable queue to match the default
         config.
         """
-        if "PRECONDITION_FAILED - inequivalent arg 'durable'" in str(err):
-            LOG.info(
-                "[%s] Retrying to declare the exchange (%s) as "
-                "non durable", conn.connection_id, self.exchange_name)
-            self.exchange = kombu.entity.Exchange(
-                name=self.exchange_name,
-                type=self.type,
-                durable=False,
-                auto_delete=self.queue_auto_delete)
-            self.queue = kombu.entity.Queue(
-                name=self.queue_name,
-                channel=conn.channel,
-                exchange=self.exchange,
-                durable=False,
-                auto_delete=self.queue_auto_delete,
-                routing_key=self.routing_key,
-                queue_arguments=self.queue_arguments,
-                consumer_arguments=consumer_arguments
-            )
-            self.queue.declare()
+        LOG.info(
+            "[%s] Retrying to declare the exchange (%s) as "
+            "non durable", conn.connection_id, self.exchange_name)
+        self.exchange = kombu.entity.Exchange(
+            name=self.exchange_name,
+            type=self.type,
+            durable=False,
+            auto_delete=self.queue_auto_delete)
+        self.queue = kombu.entity.Queue(
+            name=self.queue_name,
+            channel=conn.channel,
+            exchange=self.exchange,
+            durable=False,
+            auto_delete=self.queue_auto_delete,
+            routing_key=self.routing_key,
+            queue_arguments=self.queue_arguments,
+            consumer_arguments=consumer_arguments
+        )
+        self.queue.declare()
 
     def reset_stream_offset(self):
         if not self.rabbit_stream_fanout:
@@ -492,16 +491,21 @@ class Consumer:
             try:
                 self.queue.declare()
             except amqp_ex.PreconditionFailed as err:
-                # NOTE(hberaud): This kind of exception may be triggered
-                # when a control exchange is shared between services and
-                # when services try to create it with configs that differ
-                # from each others. RabbitMQ will reject the services
-                # that try to create it with a configuration that differ
-                # from the one used first.
-                LOG.warning('[%s] Queue %s could not be declared probably '
-                            'because of conflicting configurations: %s',
-                            conn.connection_id, self.queue_name, err)
-                self._declare_fallback(err, conn, consumer_arguments)
+                if "PRECONDITION_FAILED - inequivalent arg 'durable'" in \
+                        str(err):
+                    # NOTE(hberaud): This kind of exception may be triggered
+                    # when a control exchange is shared between services and
+                    # when services try to create it with configs that differ
+                    # from each others. RabbitMQ will reject the services
+                    # that try to create it with a configuration that differ
+                    # from the one used first.
+                    LOG.warning('[%s] Queue %s could not be declared probably '
+                                'because of conflicting configurations: %s',
+                                conn.connection_id, self.queue_name, err)
+                    self._declare_fallback_nondurable(
+                        err, conn, consumer_arguments)
+                else:
+                    raise
             except amqp_ex.NotFound as ex:
                 # NOTE(viktor.krivak): This exception is raised when
                 # non-durable and non-ha queue is hosted on node that
@@ -1596,17 +1600,19 @@ class Connection:
             try:
                 exchange(self.channel).declare()
             except amqp_ex.PreconditionFailed as err:
-                # NOTE(hberaud): This kind of exception may be triggered
-                # when a control exchange is shared between services and
-                # when services try to create it with configs that differ
-                # from each others. RabbitMQ will reject the services
-                # that try to create it with a configuration that differ
-                # from the one used first.
                 if "PRECONDITION_FAILED - inequivalent arg 'durable'" \
                    in str(err):
+                    # NOTE(hberaud): This kind of exception may be triggered
+                    # when a control exchange is shared between services and
+                    # when services try to create it with configs that differ
+                    # from each others. RabbitMQ will reject the services
+                    # that try to create it with a configuration that differ
+                    # from the one used first.
                     LOG.warning("Force creating a non durable exchange.")
                     exchange.durable = False
                     exchange(self.channel).declare()
+                else:
+                    raise
             self._declared_exchanges.add(exchange.name)
 
         log_info = {'msg': msg,

releasenotes/notes/bug-2131652-70af38c59a65886e.yaml
@@ -1,0 +1,6 @@
+---
+fixes:
+  - |
+    `Bug 2131652 <https://bugs.launchpad.net/oslo.messaging/+bug/2131652>`_:
+    Fixed ignorance of Precondition Failed error by rabbitmq driver so that
+    the error is properly raised to clients.

oslo_messaging/tests/drivers/test_impl_rabbit.py
@@ -19,6 +19,7 @@ import threading
 import time
 import uuid
 
+from amqp import exceptions as amqp_ex
 import fixtures
 import kombu
 import kombu.connection
@@ -267,6 +268,23 @@ class TestRabbitDriverLoadSSLWithFIPS(test_utils.BaseTestCase):
 
 class TestRabbitPublisher(test_utils.BaseTestCase):
     @mock.patch('kombu.messaging.Producer.publish')
+    def test_send(self, fake_publish):
+        transport = oslo_messaging.get_transport(self.conf,
+                                                 'kombu+memory:////')
+        exchange_mock = mock.Mock()
+        with transport._driver._get_connection(
+                driver_common.PURPOSE_SEND) as pool_conn:
+            conn = pool_conn.connection
+            conn._publish(exchange_mock, 'msg', routing_key='routing_key')
+        exchange_mock.return_value.declare.assert_not_called()
+        fake_publish.assert_called_with(
+            'msg', expiration=None,
+            mandatory=False,
+            compression=self.conf.oslo_messaging_rabbit.kombu_compression,
+            exchange=exchange_mock,
+            routing_key='routing_key')
+
+    @mock.patch('kombu.messaging.Producer.publish')
     def test_send_with_timeout(self, fake_publish):
         transport = oslo_messaging.get_transport(self.conf,
                                                  'kombu+memory:////')
@@ -285,14 +303,53 @@ class TestRabbitPublisher(test_utils.BaseTestCase):
             routing_key='routing_key')
 
     @mock.patch('kombu.messaging.Producer.publish')
-    def test_send_no_timeout(self, fake_publish):
+    def test_send_declare(self, fake_publish):
         transport = oslo_messaging.get_transport(self.conf,
                                                  'kombu+memory:////')
         exchange_mock = mock.Mock()
+        exchange_mock.passive = False
         with transport._driver._get_connection(
                 driver_common.PURPOSE_SEND) as pool_conn:
             conn = pool_conn.connection
             conn._publish(exchange_mock, 'msg', routing_key='routing_key')
+        exchange_mock.return_value.declare.assert_called_once_with()
+        fake_publish.assert_called_with(
+            'msg', expiration=None,
+            mandatory=False,
+            compression=self.conf.oslo_messaging_rabbit.kombu_compression,
+            exchange=exchange_mock,
+            routing_key='routing_key')
+
+    @mock.patch('kombu.messaging.Producer.publish')
+    def test_send_declare_precondition_failed(self, fake_publish):
+        transport = oslo_messaging.get_transport(self.conf,
+                                                 'kombu+memory:////')
+        exchange_mock = mock.Mock()
+        exchange_mock.passive = False
+        exchange_mock.return_value.declare.side_effect = \
+            amqp_ex.PreconditionFailed('error')
+        with transport._driver._get_connection(
+                driver_common.PURPOSE_SEND) as pool_conn:
+            conn = pool_conn.connection
+            self.assertRaises(
+                amqp_ex.PreconditionFailed,
+                conn._publish, exchange_mock, 'msg', routing_key='routing_key')
+        fake_publish.assert_not_called()
+
+    @mock.patch('kombu.messaging.Producer.publish')
+    def test_send_declare_precondition_failed_durable(self, fake_publish):
+        transport = oslo_messaging.get_transport(self.conf,
+                                                 'kombu+memory:////')
+        exchange_mock = mock.Mock()
+        exchange_mock.passive = False
+        exchange_mock.return_value.declare.side_effect = [
+            amqp_ex.PreconditionFailed(
+                "PRECONDITION_FAILED - inequivalent arg 'durable'"), None]
+        with transport._driver._get_connection(
+                driver_common.PURPOSE_SEND) as pool_conn:
+            conn = pool_conn.connection
+            conn._publish(exchange_mock, 'msg', routing_key='routing_key')
+        self.assertEqual(2, exchange_mock.return_value.declare.call_count)
         fake_publish.assert_called_with(
             'msg', expiration=None,
             mandatory=False,