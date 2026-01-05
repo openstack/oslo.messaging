cc15b88d3ce1ed0753752d5f7630fc3bf66bdf4f
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-01-06 01:11:07 +0900

    Remove old hacking check for oslo namespace package
    The oslo namespace package was removed long ago and can't be used
    any more.
    
    Change-Id: I12dc48e66b5a381eb445d74beb04b943acb906e2
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- oslo_messaging/hacking/checks.py
+++ oslo_messaging/hacking/checks.py
@@ -12,36 +12,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import re
-
 import ast
-from hacking import core
-
-
-oslo_namespace_imports_dot = re.compile(r"import[\s]+oslo[.][^\s]+")
-oslo_namespace_imports_from_dot = re.compile(r"from[\s]+oslo[.]")
-oslo_namespace_imports_from_root = re.compile(r"from[\s]+oslo[\s]+import[\s]+")
-mock_imports_directly = re.compile(r"import[\s]+mock")
-mock_imports_direclty_from = re.compile(r"from[\s]+mock[\s]+import[\s]+")
-
-
-@core.flake8ext
-def check_oslo_namespace_imports(logical_line):
-    if re.match(oslo_namespace_imports_from_dot, logical_line):
-        msg = ("O321: '%s' must be used instead of '%s'.") % (
-            logical_line.replace('oslo.', 'oslo_'),
-            logical_line)
-        yield (0, msg)
-    elif re.match(oslo_namespace_imports_from_root, logical_line):
-        msg = ("O321: '%s' must be used instead of '%s'.") % (
-            logical_line.replace('from oslo import ', 'import oslo_'),
-            logical_line)
-        yield (0, msg)
-    elif re.match(oslo_namespace_imports_dot, logical_line):
-        msg = ("O321: '%s' must be used instead of '%s'.") % (
-            logical_line.replace('import', 'from').replace('.', ' import '),
-            logical_line)
-        yield (0, msg)
 
 
 class BaseASTChecker(ast.NodeVisitor):
